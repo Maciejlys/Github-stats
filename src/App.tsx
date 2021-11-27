c4d883ddb22519fb30c05db3ede8df0fd23d9ff2
Maciejlys <maciej.lyszczarz98@gmail.com>
2021-11-27 12:19:18 +0100

    fetching working properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Flex } from "./Styled-components/Flex";
+import { MainStyle } from "./Styled-components/MainStyle";
 
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <MainStyle>
+      <Flex>
+        <h1>test</h1>
+      </Flex>
+    </MainStyle>
+  );
 }
 
 export default App;
---
+++ src/context/context.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import userMock from "./mockedData/user.json";
+import reposMock from "./mockedData/repos.json";
+import followersMock from "./mockedData/followers.json";
+
+const url = "https://course-api.netlify.app/api/react-useReducer-cart-project";
+
+const AppContext = React.createContext({});
+
+const AppProvider = ({ children }: any) => {
+  const [user, setUser] = useState(userMock);
+  const [repos, setRepos] = useState(reposMock);
+  const [followers, setFollowers] = useState(followersMock);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const rootUrl = "https://api.github.com";
+
+  const searchGithubUser = async (user: string) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+    if (response) {
+      setUser(response.data);
+      const { login, followers_url } = response.data;
+
+      await Promise.allSettled([
+        axios(`${rootUrl}/users/${login}/repos?per_page=100`),
+        axios(`${followers_url}?per_page=100`),
+      ])
+        .then((results) => {
+          const [repos, followers] = results;
+          const status = "fulfilled";
+          if (repos.status === status) {
+            setRepos(repos.value.data);
+          }
+          if (followers.status === status) {
+            setFollowers(followers.value.data);
+          }
+        })
+        .catch((err) => console.log(err));
+    } else {
+      toggleError(true, "there is no user with that username");
+    }
+    setIsLoading(false);
+  };
+
+  const toggleError = (show = false, msg = "") => {
+    setError({ show, msg });
+  };
+  useEffect(() => {
+    console.log(user);
+    // searchGithubUser("maciejlys");
+  }, []);
+
+  return (
+    <AppContext.Provider
+      value={{
+        user,
+        repos,
+        followers,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppProvider, AppContext };
